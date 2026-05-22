59156c59688b0f19c9d76d04005456d7ac5f47ab
sxtrng <sxtrng@gmail.com>
2026-05-22 05:18:17 -0400

    Creating and Initializing Object From Class.

---
+++ CreateInitializeOne.java
@@ -0,0 +1,17 @@
+public class CreateInitializeOne {
+    public static void main(String[] args) {
+
+        //  Creating an object named thing.  Instantiation of the Thing class created below.
+        Thing thing = new Thing();
+
+    }
+}
+
+
+// Creating a class named Thing.  No access modifier allowed if declared in Main.
+// Presently it does nothing.
+class Thing {
+
+
+
+}
